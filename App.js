556fbf5ea6c1b44df58e5e4e2ead6380058342e0
Thomas <jakubowski.thomas@hotmail.com>
2024-12-04 15:32:54 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
 
-export default function App() {
+// Écran d'accueil
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Accueil</Text>
+      <Button
+        title="Aller à l'écran de détails"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Écran de détails
+function DetailsScreen() {
+  const [cocktail, setCocktail] = useState(null);
+  const [cocktailName, setCocktailName] = useState('');
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`);
+      const data = await response.json();
+      if (data.drinks) {
+        setCocktail(data.drinks[0]);
+        console.log(data);
+      } else {
+        setCocktail(null);
+        console.log(data);
+        alert(data);
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Error fetching cocktail data');
+    }
+  };
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Détails</Text>
+      <Button title="Search" onPress={handleSearch} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
